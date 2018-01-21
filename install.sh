f4ce801f904ab6bfd47a2de261e09ebb3ed44eeb
Drach Py <drachpy@gmail.com>
2018-01-21 21:49:43 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,17 @@ mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
 pacstrap /mnt base
 genfstab -p /mnt >> /mnt/etc/fstab
+
 # COPY prep install
 wget https://raw.githubusercontent.com/drachpy/archlinux/master/install-prep.sh
 mv install-prep.sh /mnt/install.sh
 chmod a+x /mnt/install.sh
+
+# COPY post install
+wget https://raw.githubusercontent.com/drachpy/archlinux/master/install-post-1.sh
+cp install-post-1.sh /mnt/home/root/install-post.sh
+chmod a+x /mnt/home/root/install-post.sh
+
 # RUN prep install
 arch-chroot /mnt ./install.sh
 rm /mnt/install.sh
