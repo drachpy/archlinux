62e59c048aa15de04070872aa4b7e03ee89706c6
Drach Py <drachpy@gmail.com>
2018-01-21 20:20:17 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/drachpy/archlinux/master/install-prep.sh
-mv install-prep.sh /mnt/install.sh
-chmod a+x /mnt/install.sh
-
 timedatectl set-ntp true
 vi /etc/pacman.d/mirrorlist
 printf 'n\np\n\n\n\nw\n' | fdisk /dev/sda
@@ -12,6 +8,11 @@ mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
 pacstrap /mnt base
 genfstab -p -U /mnt >> /mnt/etc/fstab
+# COPY prep install
+wget https://raw.githubusercontent.com/drachpy/archlinux/master/install-prep.sh
+mv install-prep.sh /mnt/install.sh
+chmod a+x /mnt/install.sh
+# RUN prep install
 arch-chroot /mnt ./install.sh
 rm /mnt/install.sh
 umount /mnt
