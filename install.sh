54cca94023d738f413aac10e58f3fbdf3d5dfe68
Drach Py <drachpy@gmail.com>
2018-01-21 18:20:25 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# set /etc/pacman.d/mirrorlist to the closest repo
-
+timedatectl set-ntp true
 vim /etc/pacman.d/mirrorlist
 printf 'n\np\n\n\n\nw\n' | fdisk /dev/sda
 #parted -a optimal /dev/sda mkpart primary 0% 100% -s
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
 pacstrap /mnt base
-genfstab -p /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 wget https://raw.githubusercontent.com/drachpy/archlts/master/install-prep.sh
 mv install-prep.sh /mnt/install.sh
+chmod a+x /mnt/install.sh
 arch-chroot /mnt
