c71bbcb0d7f1cb6109149b402035ad7c0f03d74a
Drach Py <drachpy@gmail.com>
2018-01-21 15:07:38 +1300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set /etc/pacman.d/mirrorlist to the closest repo
+
+parted -a optimal /dev/sda mkpart optimal 0% 100% -s
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
+pacstrap /mnt base
+pacman -S base
+genfsstab -p /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Pacific/Auckland /etc/localtime
+echo "KEYMAP=us" >> /etc/vconsole.conf
+echo "LANGUAGE=en_US.UTF-8" >e /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+locale-gen
+echo "dracha" > /etc/hostname
+echo "127.0.0.1    localhost    localhost" >> /etc/hosts
+echo "::1          localhost    localhost" >> /etc/hosts
+mkinitcpio -p linux
+pacman -S syslinux
+syslinux-install_update -i -a -m
+sed -i 's/\/dev\/sda3/\/dev\/sda1/' /boot/syslinux/syslinux.cfg
+passwd
+poweroff
