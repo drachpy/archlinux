96b671f1b306e2128e61f81352a97d1730d04268
Drach Py <drachpy@gmail.com>
2018-01-21 16:04:38 +1300

    Create install-2.sh

---
+++ install-2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Pacific/Auckland /etc/localtime
+hwclock --systohc
+echo "KEYMAP=us" >> /etc/vconsole.conf
+echo "LANGUAGE=en_US.UTF-8" > /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+locale-gen
+echo "dracha" > /etc/hostname
+echo "127.0.0.1    localhost    localhost" >> /etc/hosts
+echo "::1          localhost    localhost" >> /etc/hosts
+mkinitcpio -p linux
+pacman -S syslinux
+syslinux-install_update -i -a -m
+sed -i 's/\/dev\/sda3/\/dev\/sda1/' /boot/syslinux/syslinux.cfg
+passwd
+#poweroff
