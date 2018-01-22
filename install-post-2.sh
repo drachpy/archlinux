f6ee3a8323ce8b8342b684506227e3fbfcc1c362
Drach Py <drachpy@gmail.com>
2018-01-22 20:41:17 +1300

    Update install-post-2.sh

--- install-post-2.sh
+++ install-post-2.sh
@@ -7,9 +7,11 @@ cd ~/.source/aur
 git clone https://aur.archlinux.org/st.git
 git clone https://aur.archlinux.org/dwm.git
 cd ~/.source/aur/st
+echo " -> Installing st"
 makepkg -si
 cd ~/.source/aur/dwm
 sed -i 's/\"st\"/\"xfce4-terminal\"/' config.h
+echo " -> Installing dwm"
 makepkg -si --skipchecksums
 
 echo "==> Installing PIP2 and PIP3"
@@ -17,10 +19,12 @@ mkdir -p ~/.source/github
 cd ~/.source/github
 git clone https://www.github.com/pypa/get-pip.git
 cd ~/.source/github/get-pip/2.6
-python2 get-pip.py
+echo " -> Installing pip2"
+sudo python2 get-pip.py
 cd ~/.source/github//get-pip/3.2
-python3 get-pip.py
+echo " -> Installing pip3"
+sudo python3 get-pip.py
 
-cd~
+cd ~
 echo "exec dwm" > ~/.xinitrc
 startx
